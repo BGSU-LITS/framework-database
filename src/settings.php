92e1fe124f5d3457fe016f20cafda2e21d1777e1
John Kloor <john@kloor.org>
2021-12-14 14:29:30 -0500

    Add templates for tables and pagination.

--- src/settings.php
+++ src/settings.php
@@ -3,8 +3,15 @@
 declare(strict_types=1);
 
 use Lits\Config\DatabaseConfig;
+use Lits\Config\TemplateConfig;
 use Lits\Framework;
 
 return function (Framework $framework): void {
     $framework->addConfig('database', new DatabaseConfig());
+
+    $settings = $framework->settings();
+    assert($settings['template'] instanceof TemplateConfig);
+
+    $path = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+    $settings['template']->paths[] = $path . 'template';
 };
